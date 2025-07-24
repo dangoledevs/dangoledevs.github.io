21915967ec828a3ceaa05a82e75db26b0ebc8b96
matt <dangoledevs@gmail.com>
2025-07-23 22:13:11 -0500

    Deploying from deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
-cd dangoledevs
+SCRIPT_DIR=$(dirname "$0")
+
+cd $SCRIPT_DIR/dangoledevs
 npm run build
-cd ..
+cd $SCRIPT_DIR
 git add .
 git commit -m "Deploying from deploy script"
 git subtree push --prefix dangoledevs/out origin gh-pages
